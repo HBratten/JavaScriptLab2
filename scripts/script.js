30549d27223b6288ad268183ac2790ac007bf8c6
Hillary <uptonh13@gmail.com>
2019-07-23 15:42:37 -0400

    Finish while loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
-const randomDamage = () => Math.floor(Math.random() * 10 + 1);
-// const chooseOption = (opt1, opt2) => const randNum Math.floor((Math.random() * 1) + 0); const randNum = 0 ? opt1 : opt2; COME BACK TO THIS!!!
-const attackPlayer = function(health) {
-  return health - randomDamage;
+const randomDamage = () => {
+  return Math.floor(Math.random() * 10 + 1);
 };
-const logHealth = (player, health) =>
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.round(Math.random());
+  return randNum === 0 ? opt1 : opt2;
+};
+const attackPlayer = function(health) {
+  return health - randomDamage();
+};
+const logHealth = (player, health) => {
   console.log(`${player} health: ${health}`);
+};
 const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
 const isDead = health => {
   if (health <= 0) {
@@ -14,9 +20,28 @@ const isDead = health => {
   }
 };
 function fight() {
-  player1: Hillary;
-  player2: Connor;
-  player1Health: 100;
-  player2Health: 100;
-  while (true) {}
+  const player1 = "Hillary";
+  const player2 = "Connor";
+  let player1Health = 100;
+  let player2Health = 100;
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      } else {
+        player1Health = attackPlayer(player1Health);
+        logHealth(player1, player1Health);
+        if (isDead(player1Health) === true) {
+          logDeath(player2, player1);
+          break;
+        }
+      }
+    }
+  }
 }
+
+fight();
